dd965cee17642846bb32f2e157fa03fbc0e3188c
kairosite <shallikerm@hotmail.co.uk>
2020-08-27 16:48:30 +0100

    proof of concept and score sorting

--- heart.py
+++ heart.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
 import discord
+from collections import defaultdict
 from config import config
 
 client = discord.Client()
-scores = dict()
+scores = defaultdict(lambda: 0)
 
 
 def pretty_format_scores() -> str:
-    pass
+    formatted_string = "```\n"
+    entries = scores.items()
+    entries = sorted(entries, key=lambda entry: entry[1], reverse=True)
+    for user, score in entries:
+        score_entry = f"{score:3} | {user.display_name} \n"
+        formatted_string += score_entry
+
+    return formatted_string + "```"
 
 
 @client.event
@@ -22,7 +30,21 @@ async def on_message(message):
         return
 
     if message.content.lower().startswith("!update"):
-        pass
+        try:
+            split_message = message.content.split()
+            score = int(split_message[1])
+        except ValueError:
+            await message.channel.send(
+                message.author.mention +
+                " fucked up, scores must be whole numbers."
+            )
+        except IndexError:
+            await message.channel.send(
+                message.author.mention +
+                ", you have to actually give a score. Muppet."
+            )
+        else:
+            scores[message.author] += score
 
     if message.content.lower() == "!get":
         await message.channel.send(
