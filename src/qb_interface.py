ebca6b53ef4c35618fca2ac787abe3958f27101d
kairosite <shallikerm@hotmail.co.uk>
2021-03-14 17:05:02 +0000

    added cache management loops

--- src/qb_interface.py
+++ src/qb_interface.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from qb_gamecache import GameCache
 from discord.ext.commands import Cog, group, Greedy
+from discord.ext.tasks import loop
 from discord import Member
 
 
@@ -10,6 +11,12 @@ class QuizBotInterface(Cog):
 
     def __init__(self):
         self.game_cache = GameCache()
+        self.writeback_task.start()
+        self.timeout_task.start()
+
+    def cog_unload(self):
+        self.writeback_task.cancel()
+        self.timeout_task.cancel()
 
     @group(
         name="start"
@@ -23,3 +30,11 @@ class QuizBotInterface(Cog):
     )
     async def start_game(self, ctx, players: Greedy[Member]):
         await self.start(ctx, *players)
+
+    @loop(minutes=5)
+    async def writeback_task(self):
+        self.game_cache.writeback()
+
+    @loop(minutes=1)
+    async def timeout_task(self):
+        self.game_cache.timeout()
